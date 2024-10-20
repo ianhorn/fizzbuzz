f48ee6e15246e164c2b9e312ba5e454ceed94e16
Ian Horn <ian.horn@ky.gov>
2024-10-20 17:18:44 -0400

    complete fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,10 @@
-# add your code here
-
+# print numbers 1 - 100 including 100
+for i in range(101):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 ==0:
+        print("Buzz")
+    else:
+        print(i)
